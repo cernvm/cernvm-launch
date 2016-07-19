6f639bacf8387d82aed012d258e5970047dce3ec
Petr Jirout <pejirout@github.com>
2016-07-19 11:20:43 +0200

    Building on Linux.

---
+++ prepare-linux64.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+CONFIG=Release
+BUILDDIR=build
+
+if [ ! -z "$1" ]; then
+	[ "$1" != "Debug" -a "$1" != "Release" ] && echo "Configuration can either be 'Debug' or 'Release'!" && exit 1  
+	CONFIG=$1
+	shift
+fi
+
+echo "Using following configuration: $CONFIG"
+
+[ ! -d $BUILDDIR ] && mkdir $BUILDDIR
+cd $BUILDDIR
+#TODO add -Wno-dev flag to the release version
+cmake .. -DCMAKE_BUILD_TYPE=${CONFIG} -DTARGET_ARCH="x86_64" $*
