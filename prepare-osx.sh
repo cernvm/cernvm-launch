a693daf4d42d48e71ac239433234f59d81e1fb9d
Petr Jirout <pejirout@github.com>
2016-07-20 17:02:19 +0200

    Added OSX support.

---
+++ prepare-osx.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+CONFIG=Release
+BUILDDIR=build
+
+if [ ! -z "$1" ]; then
+	[ "$1" != "Debug" -a "$1" != "Release" ] && echo "Configuration can either be 'Debug' or 'Release'!" && exit 1  
+	CONFIG=$1
+	shift
+fi
+
+[ ! -d $BUILDDIR ] && mkdir $BUILDDIR
+cd $BUILDDIR
+cmake .. -DCMAKE_BUILD_TYPE=${CONFIG} -DCRASH_REPORTING=OFF -DLOGGING=OFF -DTARGET_ARCH="x86_64" \
+        -DSYSTEM_CURL=ON \
+        -DSYSTEM_JSONCPP=ON \
+        -DSYSTEM_ZLIB=ON \
+        -DSYSTEM_BOOST=ON -DBOOST_ROOT="/usr/local/opt/boost155/" \
+        -DCMAKE_OSX_ARCHITECTURES="x86_64" -G"Xcode" $*
