92db22eb97c0f41658db9d98191b1ca212e09e27
Petr Jirout <pejirout@github.com>
2016-08-30 18:40:17 +0200

    CI: added build command.

--- ci/build.py
+++ ci/build.py
@@ -1,33 +1,18 @@
 #!/usr/bin/env python
 
 # Cernvm-launch repository url
-REPO_URL="https://github.com/cernvm/cernvm-launch/"
+REPO_URL = "https://github.com/cernvm/cernvm-launch/"
 # Branch to clone from the repository
-BRANCH= "devel"
-# Where to clone the repository
-REPO_DIR="./cernvm-launch/"
+BRANCH = "devel"
+# Folder where the build files are created
+BUILD_DIR = "./build"
 
 
 import os, sys, subprocess
 
 
-#TODO remove the CloneRepo and REPO_DIR
-# Clones the BRANCH of cernvm-launch repository to the REPO_DIR
-def CloneRepo():
-    print("Cloning repository: %s" % REPO_URL)
-
-    cmd = ["git", "clone", "-b", BRANCH, REPO_URL, REPO_DIR]
-    out, err, ec = RunCmd(cmd)
-
-    if ec != 0:
-        PrintErr(err)
-        return False
-
-    return True
-
-
 # Run platform and machine dependant preparation/build script.
-def RunBuildScript():
+def RunPreparationScript():
     prepareScript = ""
 
     if RunningOnWin():
@@ -40,24 +25,28 @@ def RunBuildScript():
         PrintErr("Unknown OS: %s" % os.name)
         return -1
 
-    print("Running build script: %s" % prepareScript)
-    out, err, ec = RunCmd([prepareScript])
+    print("Running preparation script: %s" % prepareScript)
+    ec = RunCmd([prepareScript])
 
-    print("out:")
-    print(out)
-    print("err:")
-    print(err)
-    print("ec:")
-    print(ec)
+    return ec
+
+
+# Run the build command in the build folder.
+def RunBuildScript():
+    buildCmd = "cmake --build %s" % BUILD_DIR
+
+    print("Running build command: %s" % buildCmd)
+    ec = RunCmd([buildCmd])
 
     return ec
 
 
 # Main function, returned value is also the exit code of the script
 def Main():
-    #if not CloneRepo():
-    #    PrintErr("Unable to clone the repository")
-    #    return 1
+    ec = RunPreparationScript()
+    if ec != 0:
+        PrintErr("Error while running the preparation script")
+        return ec
 
     ec = RunBuildScript()
 
@@ -84,10 +73,11 @@ def RunningOnLinux():
 
 # Run given command (list of arguments) in the OS shell
 def RunCmd(cmdList):
-    pipe = subprocess.Popen(cmdList, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    stdout, stderr = pipe.communicate()
+    # stdin and stderr are inherited from the parent
+    pipe = subprocess.Popen(cmdList)
+    pipe.wait()
 
-    return stdout, stderr, pipe.returncode
+    return pipe.returncode
 
 
 # Prints given errMsg to stderr, followed by a newline
